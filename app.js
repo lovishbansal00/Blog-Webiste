cc409876b10e2bcb032e4f23f6d2b3f5bec6ca06
lovishbansal00 <lovish.bansal.330@gmail.com>
2023-10-09 23:02:58 +0530

    final commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const mongodb = require("mongodb");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
